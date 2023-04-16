d47f18f46fccd2cc88d9e1dd4b914fe00e181a64
shatteredash <45927008+shatteredash@users.noreply.github.com>
2023-04-15 21:12:34 -0500

    Add files via upload

---
+++ email.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+    document.getElementById('report_form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        var params = {
+            name: document.getElementById("name").value,
+            email:document.getElementById("email").value,
+            building:document.getElementById("building").value,
+            location:document.getElementsByName("inorout").value,
+            description:document.getElementById("loc").value
+
+        }
+        this.test = params;
+
+
+        emailjs.sendForm('service_0zquxn6', 'template_ufyro17', this)
+            .then(function() {
+                console.log('SUCCESS!');
+                location.replace("https://ltracy97.github.io/KUAccess/Index.html");
+            }, function(error) {
+                console.log('FAILED...', error);
+                location.replace("https://ltracy97.github.io/KUAccess/Index.html");
+            });
+    });
+
+
+}
---
+++ email_backup.js
@@ -0,0 +1,30 @@
+window.onload = function() {
+    document.getElementById('report_form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+
+        
+        var params = {
+            name: document.getElementById("name").value,
+            email:document.getElementById("email").value,
+            building:document.getElementById("building").value,
+            location:document.getElementById("inorout").value,
+            description:document.getElementById("loc").value
+
+        }
+        this.test = params;
+
+        emailjs.sendForm('service_rae4fbc', 'template_ufyro17', this)
+            .then(function() {
+                location.replace("https://ltracy97.github.io/KUAccess/Index.html");
+            }, function(error) {
+                window.alert('FAILED...', );
+            });
+    });
+
+}
+
+    
+    
+   
+
